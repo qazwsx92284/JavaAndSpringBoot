9c2d07ee765495c3dc4706bb0f8e907226bc3196
qazwsx92284 <60413186+qazwsx92284@users.noreply.github.com>
2022-10-31 20:56:36 -0700

    Update UserService.java
    viewTicket method by providing notice number(id)

--- SpringBoot/violation/src/main/java/com/min0/violation/service/UserService.java
+++ SpringBoot/violation/src/main/java/com/min0/violation/service/UserService.java
@@ -1,5 +1,6 @@
 package com.min0.violation.service;
 
+import com.min0.violation.model.Ticket;
 import com.min0.violation.repository.ViolationRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,10 @@ public class UserService {
 
     @Autowired
     ViolationRepository violationRepository;
+
+    public Ticket viewTicket(Long id) {
+        return violationRepository.getReferenceById(id);
+    }
 
 
 }
